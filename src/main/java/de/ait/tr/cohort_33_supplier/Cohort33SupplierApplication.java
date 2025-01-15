b5bb05c170a8bac7f2999fb1e7f17497b1e4431e
Galina Lasko <galina_lasko@hotmail.com>
2025-01-15 20:37:53 +0100

    Your commit message

--- src/main/java/de/ait/tr/cohort_33_supplier/Cohort33SupplierApplication.java
+++ src/main/java/de/ait/tr/cohort_33_supplier/Cohort33SupplierApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Cohort33SupplierApplication {
-
+//hi111
 	public static void main(String[] args) {
 		ProductSupplyDto productSupplyDto=new ProductSupplyDto();
 
